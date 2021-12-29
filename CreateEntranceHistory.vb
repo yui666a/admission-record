79726885ced30d2c5926094c95c718e548ee25de
yui666a <haiso666@gmail.com>
2021-12-29 15:24:54 +0900

    DBsheetから会員番号で会員情報の行を取得

--- CreateEntranceHistory.vb
+++ CreateEntranceHistory.vb
@@ -1,5 +1,6 @@
-Private Const customerIdColumn As integer = 4
-Private Const customerNameColumn As integer = 3
+Private Const customerIdColumn As Integer = 4
+Private Const customerNameColumn As Integer = 3
+Private Const sheetCustomerName As String = "会員名簿"
 
 Private Sub Worksheet_Change(ByVal Target As Range)
 ' D列に変化があった時
@@ -7,9 +8,19 @@ Private Sub Worksheet_Change(ByVal Target As Range)
     lastRowIdNum = Cells(Rows.Count, customerIdColumn).End(xlUp).Row
     lastRowNameNum = Cells(Rows.Count, customerNameColumn).End(xlUp).Row
     If lastRowIdNum > lastRowNameNum Then lastRowNum = lastRowIdNum Else lastRowNum = lastRowNameNum
-    userNum = Cells(lastRowNum, 1)
-    MsgBox lastRowNum
+    userNum = Cells(lastRowNum, 4)
     Cells(lastRowNum, 1) = Format(Date, "yyyy/mm/dd") 'column A(参拝日付)
     Cells(lastRowNum, 7) = Format(Time, "hh:mm:ss") 'column G(参拝時間)
+
+    ' 会員DBから会員番号で会員情報を取得
+    Dim sheetCustomerList As Worksheet
+    Set sheetCustomerList = Worksheets(customerSheetName)
+    Set rowNum = sheetCustomerList.Range("A:A").Find(What:=userNum, LookAt:=xlWhole)
+
+    If rowNum Is Nothing Then
+      MsgBox "みつかりませんでした。"
+    Else
+      MsgBox rowNum.Row
+    End If
   End If
 End Sub
