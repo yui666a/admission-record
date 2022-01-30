95725ab208e95295155ee9387f6d152e44d6dc28
yui666a <haiso666@gmail.com>
2022-01-31 00:30:07 +0900

    add group category

--- static-web/src/Pages/CompareLastYearStatistic.tsx
+++ static-web/src/Pages/CompareLastYearStatistic.tsx
@@ -29,12 +29,11 @@ ChartJS.register(
 );
 
 interface Props {
-  onFileLoad?: Function;
   data: Log[];
 }
 
 const today = moment();
-const ages = [
+const generationLabels = [
   "～10",
   "10～19",
   "20～29",
@@ -46,6 +45,18 @@ const ages = [
   "80～89",
   "90～",
 ];
+const backgroundColor = [
+  "#de9610",
+  "#c93a40",
+  "#f2cf01",
+  "#0074bf",
+  "#65ace4",
+  "#a0c238",
+  "#56a764",
+  "#d16b16",
+  "#cc528b",
+  "#9460a0",
+];
 const CompareLastYearStatistic = function (props: Props) {
   const [selectedMonth, setMonth] = useState(moment(today).format("YYYY/MM"));
   const moveMonth = useCallback(
@@ -55,49 +66,55 @@ const CompareLastYearStatistic = function (props: Props) {
     [selectedMonth]
   );
 
+  //2次元配列を転置
+  const transpose = (a: number[][]) => a[0].map((_, c) => a.map((r) => r[c]));
+
   let monthLabels: string[] = [];
   for (let i = -1; i <= 0; ++i) {
     monthLabels.push(moment(selectedMonth).add(i, "year").format("YYYY/MM"));
   }
+  
+  const monthlyAdmissions = monthLabels.map((month) => {
+    // 月別の参拝者数を計算
+    let count = [0, 0, 0]; // [man, woman, all]
+    props.data.forEach((log) => {
+      if (log.date.slice(0, 7) === month) {
+        switch (log.sex) {
+          case "男":
+            count[0]++;
+            break;
+          case "女":
+            count[1]++;
+            break;
+        }
+        // If gender is not stated, it also be counted.
+        count[2]++;
+      }
+    });
+    return count;
+  });
+  const genderAdmissions = transpose(monthlyAdmissions);
 
   const graphData = {
     labels: monthLabels,
     datasets: [
       {
         type: "bar" as const, // グラフタイプを指定
-        data: monthLabels.map((month) => {
-          // 月別の参拝者数を計算
-          return props.data.filter((log) => {
-            return log.date.slice(0, 7) === month && log.sex === "男";
-          }).length;
-        }),
+        data: genderAdmissions[0],
         label: "男性",
         borderColor: "rgba(54,164,235,0.8)",
         backgroundColor: "rgba(54,164,235,0.5)",
       },
       {
         type: "bar" as const, // グラフタイプを指定
-
-        data: monthLabels.map((month) => {
-          // 月別の参拝者数を計算
-          return props.data.filter((log) => {
-            return log.date.slice(0, 7) === month && log.sex === "女";
-          }).length;
-        }),
+        data: genderAdmissions[1],
         label: "女性",
         borderColor: "rgba(254,97,132,0.8)",
         backgroundColor: "rgba(254,97,132,0.5)",
       },
-
       {
         type: "line" as const, // グラフタイプを指定
-
-        data: monthLabels.map((month) => {
-          // 月別の参拝者数を計算
-          return props.data.filter((log) => {
-            return log.date.slice(0, 7) === month;
-          }).length;
-        }),
+        data: genderAdmissions[2],
         label: "全体",
         lineTension: 0,
         fill: false,
@@ -106,100 +123,124 @@ const CompareLastYearStatistic = function (props: Props) {
     ],
   };
 
-  let a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let generations = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], //last year
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], //selected year
+  ];
+  let groupLast = {};
+  let groupSelected = {};
   props.data.forEach((log) => {
-    if (log.date.slice(0, 7) === monthLabels[0]) {
-      let generation = (parseInt(log.age) / 10) | 0;
-      if (generation > 9) generation = 9;
-      a[generation]++;
+    let index = monthLabels.indexOf(log.date.slice(0, 7));
+
+    if (index !== -1) {
+      // 世代別集計
+      let generation = (parseInt(log.age) / 10) | 0; // integer
+      if (generation > 9) generation = 9; // 100+ years old is included in 90+ years old.
+      generations[index][generation]++;
+
+      // グループ別集計
+      if (index === 0) {
+        // last year
+        if (log.group in groupLast) {
+          const key = log.group as keyof typeof groupLast;
+          groupLast = { ...groupLast, [key]: groupLast[key] + 1 };
+        } else {
+          groupLast = { ...groupLast, [log.group]: 1 };
+        }
+      } else {
+        // selected year
+        if (log.group in groupSelected) {
+          const key = log.group as keyof typeof groupSelected;
+          groupSelected = { ...groupSelected, [key]: groupSelected[key] + 1 };
+        } else {
+          groupSelected = { ...groupSelected, [log.group]: 1 };
+        }
+      }
     }
   });
+  const groupLastLabels = Object.keys(groupLast);
+  const groupLastValues = Object.values(groupLast);
+  const groupSelectedLabels = Object.keys(groupSelected);
+  const groupSelectedValues = Object.values(groupSelected);
 
-  let b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-  props.data.forEach((log) => {
-    if (log.date.slice(0, 7) === monthLabels[1]) {
-      let generation = (parseInt(log.age) / 10) | 0;
-      if (generation > 9) generation = 9;
-      b[generation]++;
-    }
-  });
-
-  const graphData2 = {
-    labels: ages,
+  const generationData1 = {
+    labels: generationLabels,
     datasets: [
       {
-        data: a,
-        hoverOffset: 4,
-        backgroundColor: [
-          "#de9610",
-          "#c93a40",
-          "#f2cf01",
-          "#0074bf",
-          "#65ace4",
-          "#a0c238",
-          "#56a764",
-          "#d16b16",
-          "#cc528b",
-          "#9460a0",
-        ],
+        data: generations[0],
+        hoverOffset: 10,
+        backgroundColor: backgroundColor,
       },
     ],
   };
-  const graphData3 = {
-    labels: ages,
+  const generationData2 = {
+    labels: generationLabels,
     datasets: [
       {
-        data: b,
-        hoverOffset: 4,
-        backgroundColor: [
-          "#de9610",
-          "#c93a40",
-          "#f2cf01",
-          "#0074bf",
-          "#65ace4",
-          "#a0c238",
-          "#56a764",
-          "#d16b16",
-          "#cc528b",
-          "#9460a0",
-        ],
+        data: generations[1],
+        hoverOffset: 10,
+        backgroundColor: backgroundColor,
+      },
+    ],
+  };
+  const groupData1 = {
+    labels: groupLastLabels,
+    datasets: [
+      {
+        data: groupLastValues,
+        hoverOffset: 10,
+        backgroundColor: backgroundColor,
+      },
+    ],
+  };
+  const groupData2 = {
+    labels: groupSelectedLabels,
+    datasets: [
+      {
+        data: groupSelectedValues,
+        hoverOffset: 10,
+        backgroundColor: backgroundColor,
       },
     ],
   };
 
   return (
-    <Content>
+    <>
       <Header>
         <button onClick={() => moveMonth(-1)}>＜</button>
         <button onClick={() => moveMonth(1)}>＞</button>
       </Header>
       <Chart type="bar" data={graphData} />
-      <GenerationArea>
+      <DetailArea>
         年齢別
         <div style={{ display: "flex" }}>
           <GenerationTable>
             <span>{moment(monthLabels[0]).format("YYYY年MM月")}</span>
-            <Pie data={graphData2} />
+            <Pie data={generationData1} />
           </GenerationTable>
           <GenerationTable>
             <span>{moment(monthLabels[1]).format("YYYY年MM月")}</span>
-            <Pie data={graphData3} />
+            <Pie data={generationData2} />
           </GenerationTable>
         </div>
-      </GenerationArea>
-    </Content>
+      </DetailArea>
+      <DetailArea>
+        グループ別
+        <div style={{ display: "flex" }}>
+          <GenerationTable>
+            <span>{moment(monthLabels[0]).format("YYYY年MM月")}</span>
+            <Pie data={groupData1} />
+          </GenerationTable>
+          <GenerationTable>
+            <span>{moment(monthLabels[1]).format("YYYY年MM月")}</span>
+            <Pie data={groupData2} />
+          </GenerationTable>
+        </div>
+      </DetailArea>
+    </>
   );
 };
 export default CompareLastYearStatistic;
-
-const Content = styled.div`
-  width: calc(100vw - 200px);
-  display: flex;
-  flex-direction: column;
-  align-content: center;
-  padding: 50px;
-`;
 
 const Header = styled.div`
   display: flex;
@@ -207,11 +248,13 @@ const Header = styled.div`
   font-size: 28px;
   margin-bottom: 20px;
 `;
-const GenerationArea = styled.div`
+const DetailArea = styled.div`
   font-size: 28px;
   margin-bottom: 20px;
   width: 100%;
   margin-top: 100px;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 const GenerationTable = styled.div`
--- static-web/src/Pages/MonthlyStatistic.tsx
+++ static-web/src/Pages/MonthlyStatistic.tsx
@@ -29,12 +29,11 @@ ChartJS.register(
 );
 
 interface Props {
-  onFileLoad?: Function;
   data: Log[];
 }
 
 const today = moment();
-const ages = [
+const generationLabels = [
   "～10",
   "10～19",
   "20～29",
@@ -46,6 +45,18 @@ const ages = [
   "80～89",
   "90～",
 ];
+const backgroundColor = [
+  "#de9610",
+  "#c93a40",
+  "#f2cf01",
+  "#0074bf",
+  "#65ace4",
+  "#a0c238",
+  "#56a764",
+  "#d16b16",
+  "#cc528b",
+  "#9460a0",
+];
 const MonthlyStatistic = function (props: Props) {
   const [selectedMonth, setMonth] = useState(moment(today).format("YYYY/MM"));
   const moveMonth = useCallback(
@@ -54,116 +65,131 @@ const MonthlyStatistic = function (props: Props) {
     },
     [selectedMonth]
   );
+  //2次元配列を転置
+  const transpose = (a: number[][]) => a[0].map((_, c) => a.map((r) => r[c]));
 
   let monthLabels: string[] = [];
   for (let i = -12; i <= 0; ++i) {
     monthLabels.push(moment(selectedMonth).add(i, "month").format("YYYY/MM"));
   }
 
+  const monthlyAdmissions = monthLabels.map((month) => {
+    // 月別の参拝者数を計算
+    let count = [0, 0, 0]; // [man, woman, all]
+    props.data.forEach((log) => {
+      if (log.date.slice(0, 7) === month) {
+        switch (log.sex) {
+          case "男":
+            count[0]++;
+            break;
+          case "女":
+            count[1]++;
+            break;
+        }
+        // If gender is not stated, it also be counted.
+        count[2]++;
+      }
+    });
+    return count;
+  });
+  const genderAdmissions = transpose(monthlyAdmissions);
+
+  let generations = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let group = {};
+  props.data.forEach((log) => {
+    if (log.date.slice(0, 7) === selectedMonth) {
+      // 世代別集計
+      let generation = (parseInt(log.age) / 10) | 0; // integer
+      if (generation > 9) generation = 9; // 100+ years old is included in 90+ years old.
+      generations[generation]++;
+
+      // グループ別集計
+      if (log.group in group) {
+        const key = log.group as keyof typeof group;
+        group = { ...group, [key]: group[key] + 1 };
+      } else {
+        group = { ...group, [log.group]: 1 };
+      }
+    }
+  });
+  const groupLabels = Object.keys(group);
+  const groupValues = Object.values(group);
+
   const graphData = {
     labels: monthLabels,
     datasets: [
       {
-        type: "bar" as const, // グラフタイプを指定
-
-        data: monthLabels.map((month) => {
-          // 月別の参拝者数を計算
-          return props.data.filter((log) => {
-            return log.date.slice(0, 7) === month && log.sex === "男";
-          }).length;
-        }),
+        type: "bar" as const,
+        data: genderAdmissions[0],
         label: "男性",
         borderColor: "rgba(54,164,235,0.8)",
         backgroundColor: "rgba(54,164,235,0.5)",
       },
       {
-        type: "bar" as const, // グラフタイプを指定
-
-        data: monthLabels.map((month) => {
-          // 月別の参拝者数を計算
-          return props.data.filter((log) => {
-            return log.date.slice(0, 7) === month && log.sex === "女";
-          }).length;
-        }),
+        type: "bar" as const,
+        data: genderAdmissions[1],
         label: "女性",
         borderColor: "rgba(254,97,132,0.8)",
         backgroundColor: "rgba(254,97,132,0.5)",
       },
-
       {
-        type: "line" as const, // グラフタイプを指定
-
-        data: monthLabels.map((month) => {
-          // 月別の参拝者数を計算
-          return props.data.filter((log) => {
-            return log.date.slice(0, 7) === month;
-          }).length;
-        }),
+        type: "line" as const,
+        data: genderAdmissions[2],
         label: "全体",
-        lineTension: 0,
         fill: false,
         borderColor: "rgba(100, 100, 100, 0.5)",
       },
     ],
   };
 
-  let a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  props.data.forEach((log) => {
-    if (log.date.slice(0, 7) === selectedMonth) {
-      let generation = (parseInt(log.age) / 10) | 0;
-      if (generation > 9) generation = 9;
-      a[generation]++;
-    }
-  });
-
-  const graphData2 = {
-    labels: ages,
+  const generationData = {
+    labels: generationLabels,
     datasets: [
       {
-        data: a,
+        data: generations,
         hoverOffset: 4,
-        backgroundColor: [
-          "#de9610",
-          "#c93a40",
-          "#f2cf01",
-          "#0074bf",
-          "#65ace4",
-          "#a0c238",
-          "#56a764",
-          "#d16b16",
-          "#cc528b",
-          "#9460a0",
-        ],
+        backgroundColor: backgroundColor,
+      },
+    ],
+  };
+
+  const groupData = {
+    labels: groupLabels,
+    datasets: [
+      {
+        data: groupValues,
+        hoverOffset: 10,
+        backgroundColor: backgroundColor,
       },
     ],
   };
 
   return (
-    <Content>
+    <>
       <Header>
         <button onClick={() => moveMonth(-1)}>＜</button>
         <button onClick={() => moveMonth(1)}>＞</button>
       </Header>
       <Chart type="bar" data={graphData} />
-      <GenerationArea>
-        年齢別
-        <GenerationTable>
-          <span>{moment(selectedMonth).format("YYYY年MM月")}</span>
-          <Pie data={graphData2} />
-        </GenerationTable>
-      </GenerationArea>
-    </Content>
+      <DetailArea>
+        <div style={{ width: "100%" }}>
+          {moment(selectedMonth).format("YYYY年MM月")}
+        </div>
+
+        <Category>
+          年齢別
+          <Pie data={generationData} />
+        </Category>
+
+        <Category>
+          グループ別
+          <Pie data={groupData} />
+        </Category>
+      </DetailArea>
+    </>
   );
 };
 export default MonthlyStatistic;
-
-const Content = styled.div`
-  width: calc(100vw - 200px);
-  display: flex;
-  flex-direction: column;
-  align-content: center;
-  padding: 50px;
-`;
 
 const Header = styled.div`
   display: flex;
@@ -171,14 +197,17 @@ const Header = styled.div`
   font-size: 28px;
   margin-bottom: 20px;
 `;
-const GenerationArea = styled.div`
+
+const DetailArea = styled.div`
   font-size: 28px;
   margin-bottom: 20px;
   width: 100%;
   margin-top: 100px;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
-const GenerationTable = styled.div`
+const Category = styled.div`
   width: 50%;
   & span {
     padding: 100px;
